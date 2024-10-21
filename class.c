81745ea0a0488df721c7c2ea35e8b520d92daa32
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-10-21 12:28:13 +0300

    update

---
+++ class.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int int1,int 2
+    scanf("Enter  two numbers %d%d",&int1,&int2);
+    int num = 0;
+    for (num=0,i <=int 2)
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+         i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+        {
+        /* code */
+    }
+    
+    
+    
+
+        num+=1;
+    }
+    printf("%d",num);
+
+    return 0;
+}
