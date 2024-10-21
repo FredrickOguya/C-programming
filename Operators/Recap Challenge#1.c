10b7dbb4ca697861f37b72d2865db857084bf06a
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-10-21 16:31:22 +0300

    Recap Challenge #1

---
+++ Operators/Recap Challenge#1.c
@@ -0,0 +1,20 @@
+/*
+Recap Challenge #1
+
+
+Challenge
+
+Easy
+Write a program that
+
+Initializes two variables of type int, a and b, with the values 5 and 3
+Initializes variable c, which is the sum of a and b (what is the type for this variable?)
+Initialized variable d that holds whether a is bigger than c - 0 or 1 (what is the type for this variable?)
+
+Hints
+
+Hint 1
+Revealed
+
+c and d should be of type int
+*/
