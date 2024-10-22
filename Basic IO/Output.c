037b465794ae1ead0da34c7aa9293f4d5ec988f8
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-10-22 14:55:22 +0300

    Output

---
+++ Basic IO/Output.c
@@ -0,0 +1,40 @@
+/*
+We have already seen how to print, but now let's dive in!
+
+In programming, it's often called "printing" to output something.
+
+In C, to print something to the screen, printf is used.
+
+For example,
+
+printf("Hello");
+The above example prints "Hello" to the screen.
+
+Notice that you must wrap what you want to print with "".
+
+
+Challenge
+
+Easy
+Write a program that prints "I love C programming".
+
+
+Solution
+
+Solution
+Revealed
+
+#include <stdio.h>
+
+int main() {
+    printf("I love C programming");
+    return 0;
+};
+*/
+#include <stdio.h>
+
+int main() {
+    // Write code here
+    printf("I love C programming");
+    return 0;
+};
