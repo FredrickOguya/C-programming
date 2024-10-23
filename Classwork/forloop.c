82e62c6bc49d310ee609f7123bec49686c251019
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-10-23 17:08:55 +0300

    for loop

---
+++ Classwork/forloop.c
@@ -0,0 +1,31 @@
+/*
+for(initialization;test;update)
+{
+    statement
+}
+know the pseudocode and flowchart
+appropriate times to use each advantages and disadvantages of both;
+pascals triangle.
+*/
+#include <stdio.h>
+
+int main (){
+    int i,n;
+    /*
+    printf("enter the number of natural numbers");
+    scanf("%d",&n);
+    printf("Natural numbers\n");
+   for(int i =1;i<n;i++){
+    printf("%d\n",i);
+    }
+    */   
+        for(int i=100;i>=2;i-=2)
+        {
+            printf("%d\n",i);
+        }
+    return 0;
+ 
+}
+  
+        
+    
