c84ea501dd005f2f9a746748e3f31c4e1756f977
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-10-23 22:18:13 +0300

    palindrome

--- Classwork/palindrome.c
+++ Classwork/palindrome.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 
 int main(){
-    int n,rev=0,num,digits;
-    printf("Enter a number: ");
+    int digits,num, rev =0,n;
+    printf("Enter the test number : ");
     scanf("%d",&num);
-
-    n= num; /*stores the original number enterred by the user*/
-
+        n=num;
     do{
         digits = num%10;
         rev = rev*10+digits;
         num/=10;
-    }while(num!=0);
-    if(n==rev)
-    {
-        printf("the number %d is a palindrome",n);
-                printf("\n");
-    }else{
-        printf("The number %d is not palindrome\n",n);
-        printf("\n");
     }
-    
+    while(num!=0);
+    if(n==rev){
+        printf("%d is a palindrome",n);
+    }else{
+        printf("%d is not a palindrome",n);
+    }
     return 0;
 }
