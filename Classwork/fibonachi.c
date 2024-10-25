0c9e9f887364a8778e6cc64e9c521a062d8830b9
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-10-25 05:03:07 +0300

    My own fibonachi

--- Classwork/fibonachi.c
+++ Classwork/fibonachi.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
 
-
-int main(){
+int main (){
     int first=0,second=1,next,n;
-    printf("enter a number\n");
+    printf("Enter the number of terms: ");
     scanf("%d",&n);
-
-    for(int i =0;i<n;i++){
-        if(i<=1)
-        {
-            next=i;
-            
-        }else
-        {
-            next = first +second;
-            first = second;
-            second=next;
-        }
+    printf("%d\n%d\n",first,second);
+    do{
+        
+        next = first + second;
+        first=second;
+        second=next;
+        n--;
         printf("%d\n",next);
 
-    }
-    printf("\n");
+    }while(n>0);
     return 0;
-
 }
