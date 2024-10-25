7416e3899b9e6a3239f09bfc2a22e9c41ea7ae80
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-10-25 06:48:18 +0300

    Comparison Operators update

--- Operators/Comparison Operators.c
+++ Operators/Comparison Operators.c
@@ -73,6 +73,6 @@ int main() {
     int n2 = 9;
     int n3 = n1==n2;
     // Don't change below this line
-    printf("n1 = %d, n2 = %d, n3 = %d", n1, n2, n3);
+    printf("n1 = %d\nn2 = %d\nn3 = %d\n", n1, n2, n3);
     return 0;
 };
