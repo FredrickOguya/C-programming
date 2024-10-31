292a9f7c34a5d54cd9712b51ce3b6eb992e26c8f
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-10-31 18:15:37 +0300

    advanced calculator

--- Final Challenges/Advanced Calculator.c
+++ Final Challenges/Advanced Calculator.c
@@ -1,20 +1,24 @@
-/*
-Challenge
-
-Write a program that acts as a calculator as in the previous lessons, but now it will ger input from the user until he enters e.
-Each input from the user will be separated by a new line(/n).
-
-The calculation will be int he format operation float (separated by space), where the possible operations are +. - ,*,//
-Each output (calculation result) should also be separated by a new line.
-
-Check the test cases for examples
-*/
-
 #include <stdio.h>
 
 int main(){
-    int ch= getchar();
-    int opp='e',num1,num2;
-    if ()
+    char op = ' ';
+    float num1;
+    float num2;
 
+    float answer;
+    while(op !='e'){
+        scanf(" %c %f %f",&op,&num1,&num2);
+        if (op == 'e'){
+            break;
+        }else if (op == '+'){
+            answer=num1+num2;
+        }else if (op == '-'){
+            answer = num1-num2;
+        }else if(op=='/'){
+            answer = num1/num2;
+        }else if(op =='*'){
+            answer= num1*num2;
+        }
+        printf("%f\n",answer);
+    }
 }
