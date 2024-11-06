66cea7178fce8b65c3118c96227bc8cd2c855410
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-11-06 07:45:02 +0300

    real root finder rehearse

--- Classwork/real root finder.c
+++ Classwork/real root finder.c
@@ -2,15 +2,21 @@
 #include <math.h>
 
 int main(){
-    int a,b,c,d,a1,a2;
+    int a,b,c,d,x,x1,x2;
 
-    printf("Enter the three variables a,b and c:\n");
-    scanf("%d %d %d",&a,&b,&c);
+    printf("Enter the coefficients a,b,and c: \n");
+    scanf("%d%d%d",&a,&b,&c);
 
-    d= b*b-4*a*c;
+    d = b*b-4*a*c;
 
-    printf("%d",d);
-
-return 0;
-
+    if (d>0){
+        x1=(-b+sqrt(d))/2*a;
+        x2 = (-b-sqrt(d))/2*a;
+        printf("x1 = %d\nx2=%d",x1,x2);
+    }else if (d==0){
+        x=-b/2*a;
+        printf("x=%d",x);
+    }else if(d<0){
+        printf("No real roots");
+    }
 }
